e86c07bc0899f82d8feab6adaac26e395a745876
Agustin Rolando <138535452+Agustin144@users.noreply.github.com>
2023-08-03 19:53:31 +0000

    Generador de cartas beta

--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // function generarCarta() {
+  let palos = [
+    { s: "♦", color: "red" },
+    { s: "♥", color: "red" },
+    { s: "♠", color: "black" },
+    { s: "♣", color: "black" }
+  ];
+  let valores = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let paloElegido = palos[Math.floor(Math.random() * palos.length)];
+  let valorElegido = valores[Math.floor(Math.random() * valores.length)];
+
+  document.querySelector(".valor").innerHTML = valorElegido;
+  document.querySelectorAll(".palo").forEach((element, index) => {
+    element.innerHTML = paloElegido.s;
+    element.style.color = paloElegido.color;
+  });
+  let width = document.getElementById("widthInput").value;
+  let height = document.getElementById("heightInput").value;
+
+  // Aplicar el ancho y alto a la carta
+  document.getElementById("carta").style.width = width + "px";
+  document.getElementById("carta").style.height = height + "px";
 };
+setInterval(window.onload, 10000);
